d7bc7ac3832a2a84ce81b45f6e9f3701ea599665
Karthik Sivakoti <sivakotikarthik@gmail.com>
2025-01-15 16:45:06 -0600

    Delete backend/app/db/database.py

--- backend/app/db/database.py
+++
@@ -1,8 +0,0 @@
-# app/db/database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import os
-
-DATABASE_URL = os.getenv('DATABASE_URL', "postgresql://postgres:postgres123@localhost:5432/crash_analytics")
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
